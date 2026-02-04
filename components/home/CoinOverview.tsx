0e60ce8d0e37a48027e95ac2d354d02455fa601c
Omar Khalaf <khalaf.omar20@gmail.com>
2026-02-05 00:52:06 +0200

    created candlestick chart component

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,37 +1,47 @@
 import React from "react";
 import { fetcher } from "@/lib/coingekco.actions";
 import Image from "next/image";
-import { CoinOverviewFallback } from "./Fallback";
 import { formatCurrency } from "@/lib/utils";
+import { CoinOverviewFallback } from "./Fallback";
+import CandlestickChart from "../CandleStickChart";
 
 const CoinOverview = async () => {
-  let coin;
-
   try {
-    coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-      dex_pair__format: "symbol",
-    });
+    const [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+      fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 1,
+        precision: "full",
+      }),
+    ]);
+
+    return (
+      <div id="coin-overview">
+        <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+          <div className="header pt-2">
+            <Image
+              src={coin.image.large}
+              alt={coin.name}
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+            </div>
+          </div>
+        </CandlestickChart>
+      </div>
+    );
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching coin overview:", error);
     return <CoinOverviewFallback />;
   }
-
-  return (
-    <div id="coin-overview">
-      <div className="header pt-2">
-        <Image
-          src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
-          alt="Bitcoins logo"
-          width={56}
-          height={56}
-        />
-      </div>
-      <div className="info">
-        <p>Bitcoin / BTC</p>
-        <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
-      </div>
-    </div>
-  );
 };
 
 export default CoinOverview;
